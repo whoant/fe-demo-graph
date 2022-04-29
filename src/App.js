77d1aa614cf1d53b94264df1faa7852fe45caa28
whoant <vovanhoangtuan4.2@gmail.com>
2022-04-29 07:44:22 +0700

    Remove cache

--- src/App.js
+++ src/App.js
@@ -9,43 +9,55 @@ import 'bootstrap/dist/css/bootstrap.css';
 import Form from "./components/Form";
 
 const httpLink = new HttpLink({
-	uri: 'http://localhost:8080/graphql'
+    uri: 'http://localhost:8080/graphql'
 });
 
 const wsLink = new GraphQLWsLink(createClient({
-	url: 'ws://localhost:8080/graphql',
+    url: 'ws://localhost:8080/graphql',
 }));
 
 const splitLink = split(
-	({ query }) => {
-		const definition = getMainDefinition(query);
-		return (
-			definition.kind === 'OperationDefinition' &&
-			definition.operation === 'subscription'
-		);
-	},
-	wsLink,
-	httpLink,
+    ({ query }) => {
+        const definition = getMainDefinition(query);
+        return (
+            definition.kind === 'OperationDefinition' &&
+            definition.operation === 'subscription'
+        );
+    },
+    wsLink,
+    httpLink,
 );
+
+const defaultOptions = {
+    watchQuery: {
+        fetchPolicy: 'no-cache',
+        errorPolicy: 'ignore',
+    },
+    query: {
+        fetchPolicy: 'no-cache',
+        errorPolicy: 'all',
+    },
+}
 
 
 const client = new ApolloClient({
-	link: splitLink,
-	cache: new InMemoryCache()
+    link: splitLink,
+    cache: new InMemoryCache(),
+    defaultOptions
 });
 
 function App() {
-	return (
-		<ApolloProvider client={client}>
-			<Container className='py-3 mt-3' style={{ backgroundColor: '' }}>
-				<h1 className='text-center text-info mb-3'>My books</h1>
-				<hr/>
-				<Form/>
-				<hr/>
-				<BookList/>
-			</Container>
-		</ApolloProvider>
-	);
+    return (
+        <ApolloProvider client={client}>
+            <Container className='py-3 mt-3' style={{ backgroundColor: '' }}>
+                <h1 className='text-center text-info mb-3'>My books</h1>
+                <hr/>
+                <Form/>
+                <hr/>
+                <BookList/>
+            </Container>
+        </ApolloProvider>
+    );
 }
 
 export default App;
